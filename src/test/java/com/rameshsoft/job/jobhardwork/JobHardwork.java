0400178d4d7ba1388b04663b7dabdf9967ac8ae9
rameshsoft9177791456 <rameshsoft.9177791456@gmail.com>
2024-12-09 20:46:52 +0530

    pushing fw pom

---
+++ src/test/java/com/rameshsoft/job/jobhardwork/JobHardwork.java
@@ -0,0 +1,12 @@
+package com.rameshsoft.job.jobhardwork;
+
+import org.testng.annotations.Test;
+
+public class JobHardwork {
+
+	@Test
+	public void hardworkJob() {
+		System.out.println("Hardwork = Job");
+	}
+	
+}
